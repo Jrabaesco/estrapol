37ae91e76eefebfb1b2c3c20a4f7413e054701ee
Jrabaesco <antony_150992@hotmail.com>
2025-04-02 09:12:15 -0500

    Configuración para producción con MongoDB Atlas

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Conectado a MongoDB Atlas'))
+  .catch(err => console.error('Error de conexión:', err));
